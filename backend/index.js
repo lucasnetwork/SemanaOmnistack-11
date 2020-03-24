3651c2c5aa9827d6654d38f8af54e72bffe83b51
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-03-24 13:42:59 -0300

    performed index.js

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const app = express()
-
-app.get('/contato',(req,res)=>{
-    res.send("<h1>Hello World</h1>")
-})
-
-app.listen(3333,() => console.log("hello world"))
