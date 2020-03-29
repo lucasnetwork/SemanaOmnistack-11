acb9795f60479e2025a13121a4c3b601939fdd29
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-03-29 10:12:06 -0300

    add remove and edit validation in ongValidation

--- backend/src/validations/OngsValidation.js
+++ backend/src/validations/OngsValidation.js
@@ -1,13 +1,30 @@
 const {celebrate,Joi,Segments} = require('celebrate')
 
 module.exports = {
-        create:celebrate({
-            [Segments.BODY]:Joi.object().keys({
-                name:Joi.string().required(),
-                email: Joi.string().required().email(),
-                whatsaap:Joi.string().required().min(10).max(11),
-                city:Joi.string().required(),
-                uf:Joi.string().required().length(2)
+    create:celebrate({
+        [Segments.BODY]:Joi.object().keys({
+            name:Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsaap:Joi.string().required().min(10).max(11),
+            city:Joi.string().required(),
+            uf:Joi.string().required().length(2)
         })
+    }),
+    edit:celebrate({
+        [Segments.BODY]:Joi.object().keys({
+            name:Joi.string().required(),
+            email:Joi.string().required().email(),
+            whatsaap:Joi.string().required().min(10).max(11),
+            city:Joi.string().required(),
+            uf:Joi.string().required().length(2)
+        }),
+        [Segments.HEADERS]:Joi.object().keys({
+            authorization:Joi.string().required(),
+        }).unknown()
+    }),
+    delete:celebrate({
+        [Segments.HEADERS]:Joi.object().keys({
+            authorization:Joi.string().required()
+        }).unknown()
     })
 }
